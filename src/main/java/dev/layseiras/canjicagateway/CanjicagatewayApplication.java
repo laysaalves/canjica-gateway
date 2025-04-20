63ae6bd0425240837015d009b07134fd8d0dc4c3
Laysa <laysa.developer@gmail.com>
2025-04-19 22:08:26 -0300

    first commit

---
+++ src/main/java/dev/layseiras/canjicagateway/CanjicagatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.canjicagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanjicagatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanjicagatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/canjicagateway/CanjicagatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.canjicagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanjicagatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
