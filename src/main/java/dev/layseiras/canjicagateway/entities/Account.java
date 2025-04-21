b0d6ead77c4a054f15ed114988239ea2e7233f75
Laysa <laysa.developer@gmail.com>
2025-04-21 09:34:40 -0300

    add account entity

---
+++ src/main/java/dev/layseiras/canjicagateway/entities/Account.java
@@ -0,0 +1,64 @@
+package dev.layseiras.canjicagateway.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tbl_account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String cpf;
+
+    public Account() {
+    }
+
+    public Account(UUID id, String name, String email, String cpf) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
+
