5dff4cf5c4eae6896404aea530df1c4b4a5ebd71
Laysa <laysa.developer@gmail.com>
2025-04-21 12:07:19 -0300

    feat: add first migration for create account table

---
+++ src/main/resources/db/migration/V1__create_account_table.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE tbl_account (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   name VARCHAR(255),
+   email VARCHAR(255) NOT NULL UNIQUE,
+   cpf VARCHAR(20) NOT NULL UNIQUE
+);
