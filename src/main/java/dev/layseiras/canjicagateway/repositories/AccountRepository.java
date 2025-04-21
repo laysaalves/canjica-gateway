a069dd1f5b25ae71015308de94972561eed5966a
Laysa <laysa.developer@gmail.com>
2025-04-21 12:08:00 -0300

    feat: add account repository with jpa

---
+++ src/main/java/dev/layseiras/canjicagateway/repositories/AccountRepository.java
@@ -0,0 +1,11 @@
+package dev.layseiras.canjicagateway.repositories;
+
+import dev.layseiras.canjicagateway.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+}
