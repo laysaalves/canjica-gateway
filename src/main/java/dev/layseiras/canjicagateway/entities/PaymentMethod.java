df9600b53d5445bb58f06bfb5cf17b8b34b393ea
Laysa <laysa.developer@gmail.com>
2025-04-23 10:57:38 -0300

    feat: create enums for payment entity

---
+++ src/main/java/dev/layseiras/canjicagateway/entities/PaymentMethod.java
@@ -0,0 +1,5 @@
+package dev.layseiras.canjicagateway.entities;
+
+public enum PaymentMethod {
+    PIX, PIX_PARCELADO
+}
---
+++ src/main/java/dev/layseiras/canjicagateway/entities/PaymentStatus.java
@@ -0,0 +1,5 @@
+package dev.layseiras.canjicagateway.entities;
+
+public enum PaymentStatus {
+    PENDING, PAID, EXPIRED, CANCELLED, REFUNDED
+}
