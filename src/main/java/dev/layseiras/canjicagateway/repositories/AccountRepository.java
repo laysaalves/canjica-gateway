c79cec68636faebd4e87decae9f68f263f306fc3
Laysa <laysa.developer@gmail.com>
2025-04-23 10:58:57 -0300

    feat: create payment by client method

--- src/main/java/dev/layseiras/canjicagateway/repositories/AccountRepository.java
+++ src/main/java/dev/layseiras/canjicagateway/repositories/AccountRepository.java
@@ -4,8 +4,10 @@ import dev.layseiras.canjicagateway.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+    List<Account> findByEmail(String email);
 }
