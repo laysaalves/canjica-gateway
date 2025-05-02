638a0a93a61a5cc0398a2688dafa5b80a70ff3dd
Layseiras <layseirasdev@outlook.com>
2025-05-02 09:37:16 -0300

    feat: add new payment to producer queue with default status

--- src/main/java/dev/layseiras/canjicagateway/service/PaymentService.java
+++ src/main/java/dev/layseiras/canjicagateway/service/PaymentService.java
@@ -2,6 +2,7 @@ package dev.layseiras.canjicagateway.service;
 
 import dev.layseiras.canjicagateway.entities.Account;
 import dev.layseiras.canjicagateway.entities.Payment;
+import dev.layseiras.canjicagateway.entities.PaymentStatus;
 import dev.layseiras.canjicagateway.repositories.AccountRepository;
 import dev.layseiras.canjicagateway.repositories.PaymentRepository;
 import org.springframework.stereotype.Service;
@@ -14,13 +15,19 @@ public class PaymentService {
 
     private final AccountRepository accountRepo;
 
-    public PaymentService(PaymentRepository repository, AccountRepository accountRepo) {
+    private final PaymentProducerService producer;
+
+    public PaymentService(PaymentRepository repository, AccountRepository accountRepo, PaymentProducerService producer) {
         this.repository = repository;
         this.accountRepo = accountRepo;
+        this.producer = producer;
     }
 
     public Payment generatePayment(Payment payment) {
-        return repository.save(payment);
+        payment.setStatus(PaymentStatus.PENDING);
+        Payment save = repository.save(payment);
+        producer.sendPayment(save);
+        return save;
     }
 
     public List<Payment> getAllPayments() {
