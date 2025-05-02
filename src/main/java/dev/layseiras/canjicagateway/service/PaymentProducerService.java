5b28de443f2827e59b4b48e18954cab3777df133
Layseiras <layseirasdev@outlook.com>
2025-05-02 09:39:59 -0300

    refactor: add mapper for send payment pending kafka topic

--- src/main/java/dev/layseiras/canjicagateway/service/PaymentProducerService.java
+++ src/main/java/dev/layseiras/canjicagateway/service/PaymentProducerService.java
@@ -1,23 +1,28 @@
 package dev.layseiras.canjicagateway.service;
 
 import dev.layseiras.canjicagateway.dtos.PaymentDTO;
+import dev.layseiras.canjicagateway.entities.Payment;
+import dev.layseiras.canjicagateway.mappers.PaymentMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentProducerService {
-    private final KafkaTemplate<String, PaymentDTO> kafkaTemplate;
 
-    public PaymentProducerService(KafkaTemplate<String, PaymentDTO> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+    private final KafkaTemplate<String, PaymentDTO> kafkaTemplate;
+    private final PaymentMapper paymentMapper;
 
     @Value("${topic.name.payment-pending:payment-pending}")
     private String paymentPendingTopic;
 
-    public void sendPayment(PaymentDTO paymentDTO) {
-        kafkaTemplate.send(paymentPendingTopic, paymentDTO);
-        System.out.println("Sent payment: " + paymentPendingTopic);
+    public PaymentProducerService(KafkaTemplate<String, PaymentDTO> kafkaTemplate, PaymentMapper paymentMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.paymentMapper = paymentMapper;
+    }
+
+    public void sendPayment(Payment payment) {
+        PaymentDTO dto = paymentMapper.toPaymentDTO(payment);
+        kafkaTemplate.send(paymentPendingTopic, dto);
     }
 }
